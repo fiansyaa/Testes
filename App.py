c107475245fa1b442030264596bfc9daf8125c48
fiansyaaa <yrafiansya@gmail.com>
2025-05-30 01:05:07 +0700

    App.py

--- App.py
+++ App.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+st.title("🌸 Aplikasi Prediksi Bunga (KNN)")
+st.markdown("""
+Selamat datang di aplikasi prediksi jenis bunga menggunakan algoritma K-Nearest Neighbors (KNN).  
+Gunakan menu di kiri untuk melakukan prediksi dan melihat hasilnya.
+""")
